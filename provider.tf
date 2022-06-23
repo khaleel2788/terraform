dd8192fea221e28a5747b86831a883f6e7146d36
khaleel2788 <104200234+khaleel2788@users.noreply.github.com>
2022-06-23 20:49:35 +0530

    Add files via upload

---
+++ provider.tf
@@ -0,0 +1,5 @@
+provider "aws" {
+    region          = "ap-south-1"
+    access_key      = "AKIA2A4BJKONTB7HVUQA"
+    secret_key      = "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"
+}
--- terraform.tf
+++ terraform.tf
@@ -1,5 +1,5 @@
-# resource "aws_instance" "app" {
-  #  instance_type           = "t2.micro"
-   # ami                     = "ami_892347389247"
-    # avaialability_zone      = "us-west-2a"
-# }
+resource "aws_instance" "app" {
+    instance_type           = "t2.micro"
+    ami                     = "ami_892347389247"
+    avaialability_zone      = "us-west-2a"
+}
